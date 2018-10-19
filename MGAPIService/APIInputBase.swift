dd75f948374ff8654e7f58f52f4e496983c89af0
Tuan Truong <tuan188@gmail.com>
2018-10-19 11:18:34 +0700

    Fix access level

--- MGAPIService/APIInputBase.swift
+++ MGAPIService/APIInputBase.swift
@@ -61,10 +61,17 @@ extension APIInputBase: CustomStringConvertible {
 }
 
 public struct APIUploadData {
-    let data: Data
-    let name: String
-    let fileName: String
-    let mimeType: String
+    public let data: Data
+    public let name: String
+    public let fileName: String
+    public let mimeType: String
+    
+    public init(data: Data, name: String, fileName: String, mimeType: String) {
+        self.data = data
+        self.name = name
+        self.fileName = fileName
+        self.mimeType = mimeType
+    }
 }
 
 open class APIUploadInputBase: APIInputBase {
--- Podfile
+++ Podfile
@@ -1,8 +1,7 @@
 platform :ios, '9.0'
 
 target 'MGAPIService' do
-#    use_frameworks!
-    pod 'RxSwift', '4.1'
+    pod 'RxSwift', '4.1.2'
     pod 'RxAlamofire', '4.2'
     pod 'ObjectMapper', '3.3'
 end
